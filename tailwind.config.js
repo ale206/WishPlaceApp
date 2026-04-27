29e3b945ac248e1872ef0289e38fca4f6e3dd9d5
Alessio Di Salvo <alessio.disalvo@gmail.com>
2026-04-27 09:57:49 +0200

    Refactor Tailwind CSS integration and add configuration files for improved styling management

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./auth/**/*.html",
+    "./download/**/*.html",
+    "./legal/**/*.html",
+    "./assets/js/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
